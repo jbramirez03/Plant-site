9986be2b935899e55301e6f6f207716a5f27723e
temptado <jason1287712@gmail.com>
2022-07-06 18:46:05 -0400

    added function on scroll to add class to header

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,14 @@ navLink.forEach(l => l.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+function scrollHeader() {
+    const header = document.getElementById('header');
+
+    if (this.scrollY >= 100) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== QUESTIONS ACCORDION ===============*/
 
