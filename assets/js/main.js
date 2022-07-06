67a209f949f87b3749600c6b1b0a08d24a1a390d
temptado <jason1287712@gmail.com>
2022-07-06 18:58:06 -0400

    added box shadow on header upon scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ navLink.forEach(l => l.addEventListener('click', linkAction));
 function scrollHeader() {
     const header = document.getElementById('header');
 
-    if (this.scrollY >= 100) header.classList.add('scroll-header');
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
     else header.classList.remove('scroll-header');
 }
 
