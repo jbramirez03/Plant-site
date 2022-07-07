07887803ea0715bd2c7548ba37a883e509c8d550
temptado <jason1287712@gmail.com>
2022-07-07 14:24:06 -0400

    added function for showing scroll up button

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,6 +95,15 @@ window.addEventListener('scroll', scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
 
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+
+    if (this.scrollY >= 200) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollUp);
+
 
 /*=============== DARK LIGHT THEME ===============*/
 
