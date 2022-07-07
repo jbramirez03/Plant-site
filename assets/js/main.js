7c70a47d4f1241423c518850c38d0f434fe0a489
temptado <jason1287712@gmail.com>
2022-07-07 14:14:09 -0400

    added active link style with underline effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,9 +78,9 @@ function scrollActive() {
     const scrollY = window.pageYOffset;
 
     sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        const sectionId = current.getAttribute('id');
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id');
 
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
